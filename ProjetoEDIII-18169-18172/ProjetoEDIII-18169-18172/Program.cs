c157825a5dee6723cd462169ef4c769b1e8eaa92
Samuel Dias <42343502+Nikenilson@users.noreply.github.com>
2019-05-21 11:07:13 -0300

    Habemus projeto

---
+++ ProjetoEDIII-18169-18172/ProjetoEDIII-18169-18172/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoEDIII_18169_18172
+{
+    static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
