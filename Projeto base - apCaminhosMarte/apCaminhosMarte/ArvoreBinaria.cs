b6126ebccc7fcce7b5b5db14d65a9ccab498c39e
Samuel Dias <42343502+Nikenilson@users.noreply.github.com>
2019-05-23 11:25:21 -0300

    O qui ingredi omissa spe

---
+++ Projeto base - apCaminhosMarte/apCaminhosMarte/ArvoreBinaria.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apCaminhosMarte
+{
+    class ArvoreBinaria<Tipo> where Tipo : IComparable<Tipo>
+    {
+        NoArvore<Tipo> raiz;
+
+        Tipo tipo;
+
+        public ArvoreBinaria()
+        {
+            raiz = new NoArvore<Tipo>();
+        }
+        public void AdicionarDado(Tipo T)
+        {
+            
+        }
+        /*public void RemoverDado(Tipo T)
+        {
+
+        }*/
+        public Tipo BuscarDado(Tipo T)
+        {
+            Tipo ret = default(Tipo);
+
+            return ret;
+        }
+        private NoArvore<Tipo> BuscarNo()
+        {
+            NoArvore<Tipo> ret = null;
+
+            return ret;
+        }
+
+    }
+}
